0b4828866ac95ffe0a3bb81446d5b2419ae9211c
BARJS753 <sahinbarisaltinay@gmail.com>
2026-08-20 16:56:11 +0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- piyasa_v5.py
+++ piyasa_v5.py
@@ -26,7 +26,7 @@ from typing import Any, Dict, List, Optional, Tuple
 
 import requests
 
-os.environ["FRED_API_KEY"] = "bf16c2cec418fb4b295c092f40439f9a"
+# os.environ["FRED_API_KEY"] = "bf16c2cec418fb4b295c092f40439f9a"
 
 try:
     import yfinance as yf
